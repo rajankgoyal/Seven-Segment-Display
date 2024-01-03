02e7cffd2cadfa47a1518958a07c7ce61841914a
rajankgoyal <72119570+rajankgoyal@users.noreply.github.com>
2024-01-02 21:42:16 -0500

    Update display7.py
    Added support for second display. Still need to debug the code a bit.

--- display7.py
+++ display7.py
@@ -7,6 +7,11 @@ segments = [machine.Pin(pin, machine.Pin.OUT) for pin in [1, 2, 3, 4, 5, 6, 7, 8
 # Define common anode/cathode pin
 common_pin = machine.Pin(0, machine.Pin.OUT)
 
+# Define segment pins for second display
+segments_b = [machine.Pin(pin, machine.Pin.OUT) for pin in [17, 18, 19, 20, 21, 22, 26, 27]]
+# Define common anode/cathode pin for second display
+common_pin_b = machine.Pin(16, machine.Pin.OUT)
+
 # Map segments to GPIO pins
 segment_map = {
     'A': segments[0],
@@ -18,6 +23,19 @@ segment_map = {
     'G': segments[6],
     'DP': segments[7]
 }
+
+# Map segments to GPIO pins on second display
+segment_map_b = {
+    'A': segments_b[0],
+    'B': segments_b[1],
+    'C': segments_b[2],
+    'D': segments_b[3],
+    'E': segments_b[4],
+    'F': segments_b[5],
+    'G': segments_b[6],
+    'DP': segments_b[7]
+}
+
 
 def display_digit(digit):
     # Define the segments for each digit (0-9)
@@ -44,6 +62,19 @@ def display_digit(digit):
 
     # Turn on the common anode/cathode to display the digit
     common_pin.on()
+    
+    
+    # Turn off all segments on second display
+    for segment_b in segment_map_b.values():
+        segment.off()
+
+    # Turn on segments for the given digit on second display
+    for segment_key_b in digit_segments[digit]:
+        segment_map_b[segment_key_b].on()
+
+    
+    # Turn on the common anode/cathode to second display the digit
+    common_pin_b.on()
 
 
 # Main loop
